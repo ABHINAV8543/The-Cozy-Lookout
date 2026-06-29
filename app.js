63a346eba7b2e99def34d2d44de9aa62e0de478d
Abhinav Anand <abhinavanandshg@gmail.com>
2026-06-29 16:00:40 +0530

    Initial Setup

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const path = require("path");
+
+async function connectDB() {
+    await mongoose.connect("mongodb://127.0.0.1:27017/thecozylookout");
+}
+connectDB()
+.then(() => {
+    console.log("Connected to Database");
+})
+.catch((error) => {
+    console.log(error);
+});
+
+app.set("view engine", "ejs");
+app.set("path", path.join(__dirname, "/views"));
+app.use(express.static(path.join(__dirname, "/public")));
+app.use(express.urlencoded({ extended: true }));
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Server is live on port ${port}`);
+});
